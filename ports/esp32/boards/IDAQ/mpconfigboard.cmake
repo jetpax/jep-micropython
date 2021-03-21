392a1ab812303495b6214753e6adec481636889b
jetpax <jetpax@users.noreply.github.com>
2021-03-21 08:48:47 -0700

    Add board specific cmake files

---
+++ ports/esp32/boards/IDAQ/mpconfigboard.cmake
@@ -0,0 +1,11 @@
+
+set(SDKCONFIG_DEFAULTS
+    boards/sdkconfig.base
+    boards/sdkconfig.ble
+    boards/sdkconfig.spiram
+    boards/sdkconfig.240mhz
+    boards/IDAQ/sdkconfig.board
+)
+
+set(MICROPY_FROZEN_MANIFEST ${MICROPY_PORT_DIR}/boards/manifest.py)
+
